9d75b558d28aebb6a8e86b1ef7bb9b8dc3d6d4ae
JackPadalino <pada0867@gmail.com>
2022-09-27 18:41:57 -0400

    updated app.js

--- app.js
+++ app.js
@@ -5,42 +5,23 @@ const {
 } = require('./db');
 
 const {
-    getSouvenirString
-} = require('../extras/souvenir');
+    home
+} = require('./views/home')
 
 const PORT = 3000;
 
-app.get("/", async(req,res)=>{
-    const [people,places,things,souvenirs] = await Promise.all([
-        Person.findAll(),
-        Place.findAll(),
-        Thing.findAll(),
-        Souvenir.findAll({include:[Person,Place,Thing]})
-    ]);
-
-    res.send(`
-    <h1>Acme People, Places, and Things</h1>
-    <h2>People</h2>
-    <ul>
-        ${people.map(person=>
-            `<li>${person.name}</li>`).join('')}
-    </ul>
-    <h2>Places</h2>
-    <ul>
-        ${places.map(place=>
-            `<li>${place.name}</li>`).join('')}
-    </ul>
-    <h2>Things</h2>
-    <ul>
-        ${things.map(thing=>
-            `<li>${thing.name}</li>`).join('')}
-    </ul>
-    <h2>Souvenir purchases</h2>
-    <ul>
-        ${souvenirs.map(souvenir=>
-            `<li>${souvenir.person.name} purchased a ${souvenir.thing.name} from ${souvenir.place.name}</li>`).join('')}
-    </ul>
-    `)
+app.get("/", async(req,res,next)=>{
+    try{
+        const [people,places,things,souvenirs] = await Promise.all([
+            Person.findAll(),
+            Place.findAll(),
+            Thing.findAll(),
+            Souvenir.findAll({include:[Person,Place,Thing]})
+        ]);
+        res.send(home(people,places,things,souvenirs))
+    }catch(error){
+        next(error);
+    };
 });
 
 app.listen(PORT,()=>{
