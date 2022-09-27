607572edb9bfd48ffaf757c46228d6458c98e763
JackPadalino <pada0867@gmail.com>
2022-09-27 19:01:20 -0400

    added form to home.js

--- views/home.js
+++ views/home.js
@@ -15,23 +15,65 @@ function home(people,places,things,souvenirs){
     <h1>Acme People, Places, and Things</h1>
     <h2>People</h2>
     <ul>
-        ${people.map(person=>
-            `<li>${person.name}</li>`)}
+        ${people.map(person=>{
+            return `<li>${person.name}</li>`
+        }
+        )}
     </ul>
     <h2>Places</h2>
     <ul>
-        ${places.map(place=>
-            `<li>${place.name}</li>`)}
+        ${places.map(place=>{
+            return `<li>${place.name}</li>`
+        }
+        )}
     </ul>
     <h2>Things</h2>
     <ul>
-        ${things.map(thing=>
-            `<li>${thing.name}</li>`)}
+        ${things.map(thing=>{
+            return `<li>${thing.name}</li>`
+        }
+        )}
     </ul>
     <h2>Souvenir purchases</h2>
     <ul>
-        ${souvenirs.map(souvenir=>
-            `<li>${souvenir.person.name} purchased a ${souvenir.thing.name} from ${souvenir.place.name}</li>`)}
+        ${souvenirs.map(souvenir=>{
+            return `<li>${souvenir.person.name} purchased a ${souvenir.thing.name} from ${souvenir.place.name}</li>`
+        }
+        )}
+    </ul>
+    <form method='POST'>
+        <label for="personId">Person</label>
+        <select name='personId'>
+            ${people.map(person => {
+                return `
+                <option value=${person.id}>
+                    ${person.name}
+                </option>`
+            })
+            }
+        </select>
+        <label for="thingId">Thing</label>
+        <select name='thingId'>
+            ${things.map(thing => {
+                return `
+                <option value=${thing.id}>
+                    ${thing.name}
+                </option>`
+            })
+            }
+        </select>
+        <label for="placeId">Place</label>
+        <select name='placeId'>
+            ${places.map(place => {
+                return `
+                <option value=${place.id}>
+                    ${place.name}
+                </option>`
+            })
+            }
+        </select>
+        <button type="submit">Submit</button>
+    </form>
 </body>
 `;
 };
