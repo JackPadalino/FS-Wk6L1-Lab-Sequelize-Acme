729f5d9b973229228c95ef999fcccc5aed89efa4
JackPadalino <pada0867@gmail.com>
2022-09-27 22:37:06 -0400

    updated app with router

--- app.js
+++ app.js
@@ -1,61 +1,14 @@
 const express = require('express');
-const app = express();
-const {
-    Person,Place,Thing,Souvenir
-} = require('./db');
-
-const {
-    home
-} = require('./views/home')
 const methodOverride = require('method-override');
+const app = express();
+const homeRouter = require('./routes/home');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(methodOverride('_method'));
+app.use('/',homeRouter);
 
 const PORT = 3000;
-
-app.get("/", async(req,res,next)=>{
-    try{
-        const [people,places,things,souvenirs] = await Promise.all([
-            Person.findAll(),
-            Place.findAll(),
-            Thing.findAll(),
-            Souvenir.findAll({include:[Person,Place,Thing]})
-        ]);
-        res.send(home(people,places,things,souvenirs))
-    }catch(error){
-        next(error);
-    };
-});
-
-app.post('/',async(req,res,next)=>{
-    try{
-        const personId = req.body.personId;
-        const thingId = req.body.thingId;
-        const placeId = req.body.placeId;
-        await Souvenir.create({
-            personId:personId,
-            thingId:thingId,
-            placeId:placeId
-        });
-        res.redirect('/')
-    }catch(error){
-        next(error);
-    };
-});
-
-// delete a souvenir
-app.delete('/:id',async(req,res,next)=>{
-    try{
-        const souvenirId = req.params.id;
-        const souvenir = await Souvenir.findByPk(souvenirId);
-        await souvenir.destroy();
-        res.redirect('/');
-    }catch(error){
-        next(error)
-    };
-});
 
 app.listen(PORT,()=>{
     console.log(`Connected to port ${PORT}...`);
